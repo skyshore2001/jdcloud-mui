91b844f7bfc0527a0ba53b62dc60de8a6cc15945
liangjian <liangjian@oliveche.com>
2017-04-27 22:15:34 +0800

    添加用于兼容旧代码的文件 lib/jdcloud-mui-compatible.js.

--- src/mui-name.js
+++ src/mui-name.js
@@ -1,39 +1,21 @@
-jdModule("jdcloud.mui.name", JdcloudMuiName);
+jdModule("jdcloud.mui", JdcloudMuiName);
 function JdcloudMuiName()
 {
 var self = this;
 var mCommon = jdModule("jdcloud.common");
 
-window.MUI = jdModule("jdcloud.mui");
+window.MUI = self;
 $.extend(MUI, mCommon);
 
 $.each([
 	"intSort",
 	"numberSort",
-// 	"enterWaiting",
-// 	"leaveWaiting",
-// 	"makeUrl",
 	"callSvr",
 	"callSvrSync",
 	"app_alert",
-// 	"makeLinkTo",
 ], function () {
 	window[this] = MUI[this];
 });
 
-/*
-// 为了兼容
-$.each([
-	"enterWaiting",
-	"leaveWaiting",
-	"makeUrl",
-	"makeLinkTo",
-	"initPageList",
-	"initPageDetail",
-], function () {
-	window[this] = MUI[this];
-});
-window.initNavbarAndList = initPageList;
-*/
 
 }
